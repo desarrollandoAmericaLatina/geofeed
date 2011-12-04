37dcc35f1a523a481adf790732eff33841c80da4
sebastianms <sams91@gmail.com>
2011-12-04 06:59:16 -0300

    Aplicacion Rails en raiz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebGeofeed::Application.config.secret_token = '42b254b9a74cd794d729f6cb57d577d9c34b4835d91320812afd291c61b16a0792b076946a4d6ddd710ec3f05a0af8a231ad65cfbfb02bd7dbeac6ea29131463'
