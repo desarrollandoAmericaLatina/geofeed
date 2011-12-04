89410904b206e0a4bd21ad4fab88f8dd29e72eb3
Tomás Barrios <tomasbarrios@gmail.com>
2011-12-04 12:10:14 -0300

    Tip para usar columnas como ordenamiento

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -3,23 +3,27 @@ class PublicController < ApplicationController
 
 
   def search
-       @tipos_instituciones = [] + Career.all.collect(&:tipo_institucion).uniq
-       @instituciones = [] + Career.all.collect{|c| c.institucion.truncate(32)}.uniq
-       @carreras = [] + Career.all.collect{|c|c.carrera.truncate(43)}.uniq
-       @search = Career.search(params[:search])
-       @careers = @search.all.shift(10)
-       @max_range = Career.maximum("promedio_arancel")
-       @min_range = Career.minimum("promedio_arancel")
-       @map_feed_url = "http://maps.google.com/maps/api/staticmap?size=512x512&markers=size:mid|color:red|"
-       markers = ''
-       @careers.each do |career|
-         markers += "#{career.direccion_casa_central},Chile|"
-       end
-       @map_feed_url += (markers + "&mobile=true&sensor=false")
-       params[:search] ||= Hash.new
-       @search.promedio_arancel_greater_than ||= @min_range
-       @search.promedio_arancel_less_than ||= @max_range
-       @search.promedio_psu_less_than ||= 850
+    if session[:tip].nil?
+      session[:tip] = @tip = true 
+    end
+    
+    @tipos_instituciones = [] + Career.all.collect(&:tipo_institucion).uniq
+    @instituciones = [] + Career.all.collect{|c| c.institucion.truncate(32)}.uniq
+    @carreras = [] + Career.all.collect{|c|c.carrera.truncate(43)}.uniq
+    @search = Career.search(params[:search])
+    @careers = @search.all.shift(10)
+    @max_range = Career.maximum("promedio_arancel")
+    @min_range = Career.minimum("promedio_arancel")
+    @map_feed_url = "http://maps.google.com/maps/api/staticmap?size=512x512&markers=size:mid|color:red|"
+    markers = ''
+    @careers.each do |career|
+      markers += "#{career.direccion_casa_central},Chile|"
+    end
+    @map_feed_url += (markers + "&mobile=true&sensor=false")
+    params[:search] ||= Hash.new
+    @search.promedio_arancel_greater_than ||= @min_range
+    @search.promedio_arancel_less_than ||= @max_range
+    @search.promedio_psu_less_than ||= 850
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @careers }
